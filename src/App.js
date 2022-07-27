f29038db40eea36c6b7d361d68be7b6a851fc458
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-07-27 09:45:17 +0200

    app.js updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import React, { Component } from 'react';
+import "./App.css";
+import React, { Component } from "react";
 import Rules from "./components/Rules";
 import Players from "./components/Players";
 import Homepage from "./components/Homepage";
@@ -13,61 +13,54 @@ class App extends Component {
       display: "noRules",
       play: false,
       opponent: false,
-    }
+    };
   }
   handleRulesClick = () => {
-    this.setState({display: "rules"})
-  }
+    this.setState({ display: "rules" });
+  };
 
   handleRulesClose = () => {
     this.setState({ display: "noRules" });
   };
 
   handlePlayClick = () => {
-    this.setState({play: true})
-  }
-  
+    this.setState({ play: true });
+  };
+
   handleOpponentClick = () => {
-    this.setState({opponent: true})
-  }
+    this.setState({ opponent: true });
+  };
 
   render() {
-  return (
-
-    <div>
-
+    return (
       <div>
-        <Homepage />
-        <div className="rules">
-          <button className="" onClick={this.handleRulesClick}>
-            Règles du jeu
-          </button>
-          <article>
-            {this.state.display === "noRules" ? (
-              <h2></h2>
-            ) : (
-              <Rules handleRulesClose={() => this.handleRulesClose()} />
-            )}
-          </article>
+        <div>
+          <h1>Roulette Russe</h1>
+          <Homepage />
+          <div className="rules">
+            <button className="" onClick={this.handleRulesClick}>
+              Règles du jeu
+            </button>
+            <article>
+              {this.state.display === "noRules" ? (
+                <h2></h2>
+              ) : (
+                <Rules handleRulesClose={() => this.handleRulesClose()} />
+              )}
+            </article>
+          </div>
         </div>
+
+        {this.state.play ? (
+          <article>
+            <Players />
+          </article>
+        ) : (
+          <p></p>
+        )}
       </div>
-      
-
-      {this.state.play ?
-      <article>
-        <Players
-        />
-      </article>
-      :
-        <p></p>
-      }
-
-
-    </div>
-  );
+    );
+  }
 }
-
-    };
-  
 
 export default App;
