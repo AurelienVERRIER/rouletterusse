af8501e21bd5ee9adc2d9cc91d8cc88a12b80638
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-07-27 11:04:11 +0200

    Select Characters Screen on going

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,15 +1,32 @@
 import React from "react";
 
 class Homepage extends React.Component {
+  constructor() {
+    super();
+ 
+    this.state = {
+      play: false,
+    }
+  }
+  
+  handlePlayClick = () => {
+    this.setState({play: true})
+  }
+
   render() {
     return (
-      <div>
-        <ul className="choices">
-          <button>Jouer</button>
-          <button>Balles</button>
-          <button>Joueurs</button>
-        </ul>
-      </div>
+      <>
+        <div>
+          <ul className="choices">
+            {/* <button>Jouer</button> */}
+
+            <button onClick={this.props.handlePlayClick} >Jouer</button>
+            
+            {/* <button onClick={this.props.handleBallsClick} >Balles</button>
+            <button onClick={this.props.handleVersusClick} >Nombre de joueurs</button> */}
+          </ul>
+        </div>
+      </>
     );
   }
 }
